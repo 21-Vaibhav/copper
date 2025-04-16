6a016b7476d3bad2659416915040536eaa3e57b1
Vaibhav G <vaibhavvishnu456@gmail.com>
2025-04-16 15:29:42 +0530

    Add FooterBar component and update page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import FooterBar from '@/components/footer-bar/FooterBar';
 import NavigationBar from '@/components/navigation-bar/NavigationBar';
 import React from 'react'
 
@@ -7,7 +8,7 @@ const page = () => {
       <NavigationBar />
       <div className='h-48 bg-blue-500'/>
       <div className='h-48 bg-red-500'/>
-      <div className='h-28 bg-indigo-500'/>
+      <FooterBar />
     </div>
   );
 }
