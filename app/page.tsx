850f9923d9e7b7241fd8d315ca35c6adefae1f62
Vaibhav G <vaibhavvishnu456@gmail.com>
2025-04-16 15:19:15 +0530

    Add NavigationBar component and update page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import NavigationBar from '@/components/navigation-bar/NavigationBar';
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
-  )
+    <div className="flex flex-col justify-between">
+      <NavigationBar />
+      <div className='h-48 bg-blue-500'/>
+      <div className='h-48 bg-red-500'/>
+      <div className='h-28 bg-indigo-500'/>
+    </div>
+  );
 }
 
 export default page
